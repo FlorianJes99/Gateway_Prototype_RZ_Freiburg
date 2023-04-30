50c7993aa4b63b8d28e391f1cbfa6ee0f8c1adec
Florian Jessl <florian.jessl99@gmail.com>
2023-04-30 14:05:31 +0200

    gateway klass optimiert und channel klasse hinzugefuegt

--- GatewayClass.py
+++ GatewayClass.py
@@ -7,22 +7,7 @@ class Gateway:
     def __init__(self, host: str, listening_port: int):
         self.host = host
         self.listening_port = listening_port
-        self.client_groups: dict[str:ClientConnection] = {}
-
-    async def handle_connection(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-        client_ip = writer.get_extra_info('peername')
-
-        if client_ip in self.client_groups:
-            group: ChannelsClientServer = self.client_groups[client_ip]
-        else:
-            group = ChannelsClientServer(client_ip)
-            self.client_groups[client_ip] = group
-
-        connection = ClientConnection(reader, writer)
-        group.add_channel(connection)
-
-        logging.info(f'Creating task for group of channels: {group}')
-        asyncio.create_task(group.handle_channel_data('localhost', 5900))
+        self.channels = []
 
     async def start(self):
         logging.info('Starting gateway...')
@@ -35,36 +20,42 @@ class Gateway:
         async with server:
             await server.serve_forever()
 
-
-class ChannelsClientServer:
-    def __init__(self, ip_adress: str):
-        self.ip_adress = ip_adress
-        self.channels: list[ClientConnection] = []
-
-    def add_channel(self, channel):
-        self.channels.append(channel)
-
-    async def handle_channel_data(self, server_host: str, server_port: int):
-        while True:
-            for channel in self.channels:
-                server_reader, server_writer = await asyncio.open_connection(server_host, server_port)
-                logging.debug('Sending to Server...')
-                send_to_server = SendData.send_data(channel.reader, server_writer)
-                logging.debug('Sending to client...')
-                send_to_client = SendData.send_data(server_reader, channel.writer)
-                await asyncio.gather(send_to_server, send_to_client)
+    async def handle_connection(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+        client_ip = writer.get_extra_info('peername')[0]
+        logging.debug(f'Client ip: {client_ip}')
+        server_reader, server_writer = await asyncio.open_connection('localhost', 5900)
+        channel = SpiceChannel(reader, writer, server_reader, server_writer)
+        to_server = asyncio.create_task(channel.send_to_server())
+        to_client = asyncio.create_task(channel.send_to_client())
+        # await waits for ever due to server and client streams never ending
+        await asyncio.gather(to_server, to_client)
 
 
-class ClientConnection:
-    def __init__(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-        self.reader = reader
-        self.writer = writer
+class SpiceChannel:
+    def __init__(self, client_reader, client_writer, server_reader, server_writer):
+        self.client_reader = client_reader
+        self.client_writer = client_writer
+        self.server_reader = server_reader
+        self.server_writer = server_writer
+        self.channel_number = None
 
+    async def send_to_server(self):
+        await self.process_data(self.client_reader, self.server_writer)
 
-class SendData:
-    async def send_data(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    async def send_to_client(self):
+        await self.process_data(self.server_reader, self.client_writer)
+
+    async def process_data(self, reader, writer):
         try:
             while not reader.at_eof():
-                writer.write(await reader.read(4096))
+                data = await reader.read(1024)
+                if self.channel_number is None:
+                    self.channel_number = data[20]
+                    logging.debug(f'Channel number is: {self.channel_number}')
+                writer.write(data)
+                await writer.drain()
+        except Exception as e:
+            logging.error(repr(e))
         finally:
+            logging.info('Closing writer')
             writer.close()
