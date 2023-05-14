3a0d5860f42ae671d0f86f29d6b8fccf8b63dae9
Florian Jessl <florian.jessl99@gmail.com>
2023-05-14 22:16:32 +0200

    websocket support

--- GatewayClass.py
+++ GatewayClass.py
@@ -1,18 +1,44 @@
 from concurrent.futures import ProcessPoolExecutor
+from websockets.server import serve
+from websockets.client import connect
 import asyncio
 import time
 import logging
-from websockets.server import serve
 logging.basicConfig(level=logging.DEBUG)
 
-init_block = True
+INIT_BLOCK = True
 BUFFER_SIZE = 2048  # in bytes
 SLEEP_TIME = 5  # seconds
+WS_MAX_SIZE = None  # keine MAX_SIZE
+# read_limit, max_queue und write_limit veraendern fuer performance tests
 
-p = ProcessPoolExecutor(10)
+
+class GatewayWebsockets(object):
+    def __init__(self, host: str, listening_port: int, pool_executor: ProcessPoolExecutor):
+        self.host = host
+        self.listening_port = listening_port
+        self.process_pool_executor = pool_executor
+
+    async def start(self):
+        logging.info('Starting gateway...')
+        async with serve(self.handle_connection, self.host, self.listening_port, max_size=WS_MAX_SIZE):
+            await asyncio.Future()
+
+    async def handle_connection(self, ws_client):
+        args = {
+                'max_size': WS_MAX_SIZE,
+        }
+        async with connect('ws://localhost:5900', **args) as ws_server:
+            to_server = asyncio.create_task(self.proxy(ws_client, ws_server))
+            to_client = asyncio.create_task(self.proxy(ws_server, ws_client))
+            await asyncio.gather(to_server, to_client)
+
+    async def proxy(self, ws_in, ws_out):
+        async for msg in ws_in:
+            await ws_out.send(msg)
 
 
-class Gateway(object):
+class GatewaySockets(object):
     def __init__(self, host: str, listening_port: int, pool_executor: ProcessPoolExecutor, args):
         self.host = host
         self.listening_port = listening_port
@@ -21,7 +47,6 @@ class Gateway(object):
         self.args = args
 
     async def start(self):
-        logging.info('Starting gateway...')
         server = await asyncio.start_server(
           self.handle_connection_,
           self.host,
@@ -34,13 +59,13 @@ class Gateway(object):
         logging.debug(f'Client ip: {client_ip}')
         server_reader, server_writer = await asyncio.open_connection('localhost', 5900)
         channel = SpiceChannel(reader, writer, server_reader, server_writer)
-        global init_block
+        global INIT_BLOCK
         to_server = asyncio.create_task(channel.send_to_server())
-        if init_block is True:
-            logging.info('Sleeping for 30s')
+        if INIT_BLOCK is True:
+            logging.info(f'Sleeping for {SLEEP_TIME}s')
             time.sleep(SLEEP_TIME)  # simulate api request, which return location of server
             logging.info('Done sleeping')
-            init_block = False
+            INIT_BLOCK = False
         to_client = asyncio.create_task(channel.send_to_client())
         # await waits for ever due to server and client streams never ending
         await asyncio.gather(to_server, to_client)
--- main.py
+++ main.py
@@ -4,20 +4,27 @@ import GatewayClass
 import asyncio
 import argparse
 
-HOST = ''
+HOST = 'localhost'
 LISTENING_PORT = 1234
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 
 async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-w', '--websockets', action='store_true', help='Enable websocket support for the gateway')
+    parser.add_argument('-d', '--debug', action='store_true', help='Enable debugging logs')
     args = parser.parse_args()
+    p = ProcessPoolExecutor(10)
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
     if args.websockets:
         logging.info('Started gateway in websockets mode')
-    p = ProcessPoolExecutor(10)
-    gateway = GatewayClass.Gateway(HOST, LISTENING_PORT, p, args)
-    await gateway.start()
+        gateway = GatewayClass.GatewayWebsockets(HOST, LISTENING_PORT, p)
+        await gateway.start()
+    else:
+        logging.info('Started gateway in sockets mode')
+        gateway = GatewayClass.GatewaySockets(HOST, LISTENING_PORT, p)
+        await gateway.start()
 
 
 if __name__ == '__main__':
